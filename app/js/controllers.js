1cd5783e00099f426e43a9ab22767cb3ea8b8aa1
Sergio Schuler <sergio.schuler@gmail.com>
2014-06-07 10:11:28 -0300

    first commit. Already plugged with firebase and bootstrap

---
+++ app/js/controllers.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+	.controller('LandingPageController', [function() {
+
+	}])
+
+	.controller('WaitlistController', ['$scope', '$firebase', function($scope, $firebase) {
+		var partiesRef = new Firebase('https://waitandeat-sergio.firebaseio.com/');
+		
+		$scope.parties = $firebase(partiesRef);
+
+		$scope.party = {name: '', phone: '', size: ''};
+
+		$scope.saveParty = function() {
+			$scope.parties.$add($scope.party);
+			$scope.party = {name: '', phone: '', size: ''};
+		};
+	}]);

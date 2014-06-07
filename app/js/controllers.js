fa61d5ef836fba0ab25e268e71ded5d902a7b2f1
Sergio Schuler <sergio.schuler@gmail.com>
2014-06-07 13:37:09 -0300

    Implement logic for marking party as done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,20 +6,18 @@ angular.module('myApp.controllers', [])
 	.controller('LandingPageController', [function() {
 
 	}])
-
 	.controller('WaitlistController', ['$scope', '$firebase', function($scope, $firebase) {
 		// Connect $scope.parties to live Firebase data.
 		var partiesRef = new Firebase('https://waitandeat-sergio.firebaseio.com/parties');
-		
 		$scope.parties = $firebase(partiesRef);
 
 		// Object to store data from waitlist form.
-		$scope.newParty = {name: '', phone: '', size: ''};
+		$scope.newParty = {name: '', phone: '', size: '', done: false};
 
 		// Function to save a new party to the waitlist.
 		$scope.saveParty = function() {
 			$scope.parties.$add($scope.newParty);
-			$scope.newParty = {name: '', phone: '', size: ''};
+			$scope.newParty = {name: '', phone: '', size: '', done: false};
 		};
 
 		// Function to send a text message to a party

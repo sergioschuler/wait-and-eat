fbe80086dc3ac04677995761b34696462f8f9329
Sergio Schuler <sergio.schuler@gmail.com>
2014-06-07 11:00:48 -0300

    firebase organization and plumbing for sending sms

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,14 +8,24 @@ angular.module('myApp.controllers', [])
 	}])
 
 	.controller('WaitlistController', ['$scope', '$firebase', function($scope, $firebase) {
-		var partiesRef = new Firebase('https://waitandeat-sergio.firebaseio.com/');
+		// Connect $scope.parties to live Firebase data.
+		var partiesRef = new Firebase('https://waitandeat-sergio.firebaseio.com/parties');
 		
 		$scope.parties = $firebase(partiesRef);
 
+		// Object to store data from waitlist form.
 		$scope.newParty = {name: '', phone: '', size: ''};
 
+		// Function to save a new party to the waitlist.
 		$scope.saveParty = function() {
 			$scope.parties.$add($scope.newParty);
 			$scope.newParty = {name: '', phone: '', size: ''};
+		};
+
+		// Function to send a text message to a party
+		$scope.sendTextMessage = function(phoneNumber) {
+			var textMessageRef = new Firebase('https://waitandeat-sergio.firebaseio.com/textMessages');
+			var textMessages = $firebase(textMessageRef);
+			textMessages.$add({phoneNumber: phoneNumber});
 		};
 	}]);
